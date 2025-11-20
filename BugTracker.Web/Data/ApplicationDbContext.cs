c57f1b8661c42f0c80f96e4bdf63114e7894b811
JanudiRisaka <jrsemini@gmail.com>
2025-11-20 22:39:39 +0530

    Add project files.

---
+++ BugTracker.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BugTracker.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BugTracker.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // This line says: "Create a table named 'Bugs' based on the Bug class"
+        public DbSet<Bug> Bugs { get; set; }
+    }
+}
---
+++ BugTracker.Web/Models/Bug.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Web.Models
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string Priority { get; set; } = "Medium";
+
+        [Required]
+        public string Status { get; set; } = "Open";
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
